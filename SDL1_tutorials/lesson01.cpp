40d210866532e5e228f91f74cf5d5173a0dea1ea
Daniel Abbott <danielabbott@daniel-abbott.com>
2018-11-10 14:02:13 -0700

    Initial commit - Completed lessons 1 and 2 - Lesson 1 is being excluded from project so I don't have to create and set up multiple projects for each lesson

---
+++ SDL1_tutorials/lesson01.cpp
@@ -0,0 +1,37 @@
+// Include SDL1 functions and datatypes
+#include <SDL.h>
+
+#pragma comment(lib, "SDL.lib")
+#pragma comment(lib, "SDLmain.lib")
+
+int main(int argc, char* argv[]) {
+	// Surface pointers
+	SDL_Surface* hello = NULL;
+	SDL_Surface* screen = NULL;
+
+	// Start SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	// Set up screen (software surface)
+	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+
+	// Load the image
+	hello = SDL_LoadBMP("hello.bmp");
+
+	// Apply image to screen
+	SDL_BlitSurface(hello, NULL, screen, NULL);
+
+	// Update the screen
+	SDL_Flip(screen);
+
+	// Delay two seconds before closing
+	SDL_Delay(2000);
+
+	// Free the loaded image
+	SDL_FreeSurface(hello);
+
+	// Quit SDL
+	SDL_Quit();
+
+	return 0;
+}
